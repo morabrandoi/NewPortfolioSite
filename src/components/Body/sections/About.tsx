fcb951efa188230943813e591107e71e94eb0bf1
morabrandoi <morabrandoi@gmail.com>
2023-08-20 20:14:19 -0400

    lkjnl

--- src/components/Body/sections/About.tsx
+++ src/components/Body/sections/About.tsx
@@ -4,9 +4,11 @@ import { HEADSHOT_FILENAME } from '../../../constants/fileNames';
 export default function About() {
   return (
     <VStack sx={{ scrollMarginTop: '1000px' }} spacing={25} zIndex={50}>
-      <Avatar width="300px" height="300px" src={`/imgs/${HEADSHOT_FILENAME}`} />
+      <Avatar width="250px" height="250px" src={`/imgs/${HEADSHOT_FILENAME}`} />
 
-      <Heading size="2xl">I&apos;m Brando Mora</Heading>
+      <Heading size="2xl">
+        <b>I&apos;m Brando Mora</b>
+      </Heading>
       <Heading size="sm" noOfLines={2}>
         Software Engineer;
         <br />
