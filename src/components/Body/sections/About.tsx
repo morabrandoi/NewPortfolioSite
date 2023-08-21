7085b20d73c4983527f5a9a8797c50dc05fac426
morabrandoi <morabrandoi@gmail.com>
2023-08-20 20:11:53 -0400

    kj;h;u

--- src/components/Body/sections/About.tsx
+++ src/components/Body/sections/About.tsx
@@ -1,16 +1,10 @@
-import { VStack, Heading, Image } from '@chakra-ui/react';
+import { VStack, Heading, Avatar } from '@chakra-ui/react';
 import { HEADSHOT_FILENAME } from '../../../constants/fileNames';
 
 export default function About() {
   return (
     <VStack sx={{ scrollMarginTop: '1000px' }} spacing={25} zIndex={50}>
-      <Image
-        borderRadius="50px"
-        boxSize="200px"
-        objectFit="cover"
-        src={`/imgs/${HEADSHOT_FILENAME}`}
-        alt="A picture of me, Brando Mora."
-      ></Image>
+      <Avatar width="300px" height="300px" src={`/imgs/${HEADSHOT_FILENAME}`} />
 
       <Heading size="2xl">I&apos;m Brando Mora</Heading>
       <Heading size="sm" noOfLines={2}>
